5a50ee2b3d78a017917102be3b8d766826171c97
sainul-abid-tk <sainulabidtk2468@gmail.com>
2024-01-20 09:25:03 +0530

    4th

--- index.js
+++ index.js
@@ -1,40 +1,35 @@
-const express=require("express")
-const app=express()
-const http =require("http")
-const cors =require("cors")
-const PORT= process.env.PORT || 3000
-const { Server }=require("socket.io")
-app.use(cors());
-
-const server=http.createServer(app)
-const io = new Server(server,{
+const app=require('express')()
+const server=require('http').createServer(app)
+const io=require('socket.io')(server,{
     cors:{
-        origin:"http://localhost:3000",
-        methods:["GET","POST"],
-    },
+        origin:"*"
+    }
 })
+const PORT=7000||process.env.PORT
+
 app.get('/',(req,res)=>{
-    res.send("Helloo Its Working")
+    res.send("<h1>hlooo gyssss</h1>")
 })
+let users=[]
 io.on("connection",(socket)=>{
-    console.log(`User Connected:${socket.id}`);
-
-    socket.on("join_room",(data)=>{
-        socket.join(data)
-        console.log(`User With ID:${socket.id} and Joined Room is:${data}`);
+        console.log(`User Connected:${socket.id}`);
+        socket.on("join_room",(id)=>{
+            socket.join(id)
+            console.log(`User With ID:${socket.id} and Joined Room is:${id}`);
+            socket.on("join_user",(data)=>{
+                socket.to(id).emit("recive_user",data)
+            })
+        })
+        socket.on("send_message",(data)=>{
+            socket.to(data.roomId).emit("receive_message",data)
+        })
+    
+        socket.on("disconnect",()=>{
+            console.log("User Disconnected!",socket.id);
+            users=[]
+        })
     })
-
-    socket.on("send_message",(data)=>{
-        socket.to(data.roomId).emit("receive_message",data)
-    })
-
-    socket.on("disconnect",()=>{
-        console.log("User Disconnected!",socket.id);
-    })
-})
-
-
 
 server.listen(PORT,()=>{
-    console.log(`Server is Running!!at ${PORT}`);
+    console.log(`Iam Listening at port:${PORT}`);
 })
