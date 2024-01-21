ad1608925f36981468480cf8471d6ef167bfe3b7
sainul-abid-tk <sainulabidtk2468@gmail.com>
2024-01-21 14:53:46 +0530

    6th

--- index.js
+++ index.js
@@ -6,24 +6,30 @@ const io=require('socket.io')(server,{
     }
 })
 const PORT=7000||process.env.PORT
+
 app.get('/',(req,res)=>{
     res.send("<h1>hlooo gyssss</h1>")
 })
 io.on("connection",(socket)=>{
+        
         console.log(`User Connected:${socket.id}`);
+        let username=""
+        let room=""
         socket.on("join_room",(id)=>{
             socket.join(id)
+            room=id
             console.log(`User With ID:${socket.id} and Joined Room is:${id}`);
             socket.on("join_user",(data)=>{
+                username=data
                 socket.to(id).emit("recive_user",data)
             })
         })
         socket.on("send_message",(data)=>{
             socket.to(data.roomId).emit("receive_message",data)
         })
-    
         socket.on("disconnect",()=>{
-            console.log("User Disconnected!",socket.id);
+            socket.to(room).emit("left_user",username)
+            console.log("User Disconnected!",username);
         })
     })
 
